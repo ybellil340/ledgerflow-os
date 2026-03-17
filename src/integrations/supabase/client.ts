1c54303f375582b23970dd549956336051763d43
ybellil340 <ybellil340@gmail.com>
2026-03-18 00:59:07 +0100

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL || "https://szpprhwjywehidmgicsz.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6cHByaHdqeXdlaGlkbWdpY3N6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM3MTUzNzYsImV4cCI6MjA4OTI5MTM3Nn0.MysY8PkX_YgFlZqJ8QhH13AocYprR1fhOiMnUZXR3to";
+const SUPABASE_URL = "https://kffoobvffjmdblugcddl.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_TWCh-lIPxInN1PGRQ7DOZQ_cPIMrilM";
 
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
   auth: {
