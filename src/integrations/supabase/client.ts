12b1cf5c2b4031911ea8215d3350861a694273e8
ybellil340 <ybellil340@gmail.com>
2026-03-18 01:38:25 +0100

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,9 +2,9 @@ import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
 const SUPABASE_URL = "https://kffoobvffjmdblugcddl.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_TWCh-lIPxInN1PGRQ7DOZQ_cPIMrilM";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtmZm9vYnZmZmptZGJsdWdjZGRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM3Nzg1NzYsImV4cCI6MjA4OTM1NDU3Nn0.xvWFSN0lLzh0q7iq049Y_4lCSJtDzBUvkJl1oNnX5wk";
 
-export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
+export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
     storage: localStorage,
     persistSession: true,
